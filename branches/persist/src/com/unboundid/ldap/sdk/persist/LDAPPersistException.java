e0dd4287d71ebf4f3c1defec7f7778dec39d0b54
dirmgr <dirmgr@97646bd4-b361-44ce-9a60-91f5edc4e61d>
2009-10-01 18:28:58 +0000

    Update the LDAP SDK to add a new LDAPPersistException class that defines an exception that can be thrown if a problem occurs within the persistence framework.

---
+++ branches/persist/src/com/unboundid/ldap/sdk/persist/LDAPPersistException.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2009 UnboundID Corp.
+ * All Rights Reserved.
+ */
+/*
+ * Copyright (C) 2009 UnboundID Corp.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPLv2 only)
+ * or the terms of the GNU Lesser General Public License (LGPLv2.1 only)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ */
+package com.unboundid.ldap.sdk.persist;
+
+
+
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.ResultCode;
+import com.unboundid.util.NotMutable;
+import com.unboundid.util.ThreadSafety;
+import com.unboundid.util.ThreadSafetyLevel;
+
+
+
+/**
+ * This class defines an exception that may be thrown if a problem occurs while
+ * attempting to perform processing related to persisting Java objects in an
+ * LDAP directory server.
+ */
+@NotMutable()
+@ThreadSafety(level=ThreadSafetyLevel.COMPLETELY_THREADSAFE)
+public final class LDAPPersistException
+       extends LDAPException
+{
+  /**
+   * The serial version UID for this serializable class.
+   */
+  private static final long serialVersionUID = 8625904586803506713L;
+
+
+
+  /**
+   * Creates a new LDAP persist exception that wraps the provided LDAP
+   * exception.
+   *
+   * @param  e  The LDAP exception to wrap with this LDAP persist exception.
+   */
+  public LDAPPersistException(final LDAPException e)
+  {
+    super(e);
+  }
+
+
+
+  /**
+   * Creates a new LDAP persist exception with the provided message.
+   *
+   * @param  message  The message for this exception.
+   */
+  public LDAPPersistException(final String message)
+  {
+    super(ResultCode.LOCAL_ERROR, message);
+  }
+
+
+
+  /**
+   * Creates a new LDAP persist exception with the provided message and cause.
+   *
+   * @param  message  The message for this exception.
+   * @param  cause    The underlying cause for this exception.
+   */
+  public LDAPPersistException(final String message, final Throwable cause)
+  {
+    super(ResultCode.LOCAL_ERROR, message, cause);
+  }
+}
