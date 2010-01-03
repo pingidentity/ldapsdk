4abe115e7598fc7ebe9987172f46c5be618ddc36
Neil Wilson <dirmgr@users.noreply.github.com>
2010-01-03 20:46:57 +0000

    Update the LDAP SDK to change all copyright notices to include the year 2010.

--- src/com/unboundid/asn1/ASN1Integer.java
+++ src/com/unboundid/asn1/ASN1Integer.java
@@ -1,9 +1,9 @@
 /*
- * Copyright 2007-2009 UnboundID Corp.
+ * Copyright 2007-2010 UnboundID Corp.
  * All Rights Reserved.
  */
 /*
- * Copyright (C) 2008-2009 UnboundID Corp.
+ * Copyright (C) 2008-2010 UnboundID Corp.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License (GPLv2 only)
