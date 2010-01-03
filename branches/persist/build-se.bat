7b905b2f94d4ac8c33c62dd3900eb1ae774fe3f0
dirmgr <dirmgr@97646bd4-b361-44ce-9a60-91f5edc4e61d>
2010-01-03 21:54:35 +0000

    Update the LDAP SDK persistence branch to change all copyright notices to include the year 2010.

--- branches/persist/build-se.bat
+++ branches/persist/build-se.bat
@@ -1,11 +1,11 @@
 @echo off
 
-rem Copyright 2008-2009 UnboundID Corp.
+rem Copyright 2008-2010 UnboundID Corp.
 rem All Rights Reserved.
 rem
 rem -----
 rem
-rem Copyright (C) 2008-2009 UnboundID Corp.
+rem Copyright (C) 2008-2010 UnboundID Corp.
 rem This program is free software; you can redistribute it and/or modify
 rem it under the terms of the GNU General Public License (GPLv2 only)
 rem or the terms of the GNU Lesser General Public License (LGPLv2.1 only)
@@ -19,7 +19,7 @@ rem
 rem You should have received a copy of the GNU General Public License
 
 
-rem Copyright 2007-2009 UnboundID Corp.
+rem Copyright 2007-2010 UnboundID Corp.
 rem All Rights Reserved.
 
 
