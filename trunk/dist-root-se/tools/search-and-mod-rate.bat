1a22d92a382f06e654bd3b247d38d7e34aabe299
dirmgr <dirmgr@97646bd4-b361-44ce-9a60-91f5edc4e61d>
2010-04-10 22:04:37 +0000

    Update the LDAP SDK to add a new search-and-mod-rate tool that can be used to measure combined search and modify performance.  It will perform searches in much the same way as the searchrate tool (except that it doesn't support asynchronous mode), but will then modify each entry returned from the search.

---
+++ trunk/dist-root-se/tools/search-and-mod-rate.bat
@@ -0,0 +1,40 @@
+@echo off
+
+rem Copyright 2008-2010 UnboundID Corp.
+rem All Rights Reserved.
+rem
+rem -----
+rem
+rem Copyright (C) 2008-2010 UnboundID Corp.
+rem This program is free software; you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License (GPLv2 only)
+rem or the terms of the GNU Lesser General Public License (LGPLv2.1 only)
+rem as published by the Free Software Foundation.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem
+rem You should have received a copy of the GNU General Public License
+
+
+rem Get the directory containing this batch file.
+set BATDIR=%~dp0
+
+rem Figure out which Java command to invoke.
+if "%UNBOUNDID_JAVA_HOME%" == "" goto checkJavaHome
+set JAVA_CMD=%UNBOUNDID_JAVA_HOME%\bin\java.exe
+goto runTool
+
+:checkJavaHome
+if "%JAVA_HOME%" == "" goto usePath
+set JAVA_CMD=%JAVA_HOME%\bin\java.exe
+goto runTool
+
+:usePath
+set JAVA_CMD=java.exe
+
+:runTool
+"%JAVA_CMD%" %JAVA_ARGS% -cp "%BATDIR%\..\unboundid-ldapsdk-se.jar;%CLASSPATH%" com.unboundid.ldap.sdk.examples.SearchAndModRate %*
+
