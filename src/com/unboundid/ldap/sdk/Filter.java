f6c4133b206dbe72c1748687d7e354912dd5737e
Neil Wilson <dirmgr@users.noreply.github.com>
2015-01-02 07:11:43 +0000

    Update the LDAP SDK copyright notices to include the year 2015.

--- src/com/unboundid/ldap/sdk/Filter.java
+++ src/com/unboundid/ldap/sdk/Filter.java
@@ -1,9 +1,9 @@
 /*
- * Copyright 2007-2014 UnboundID Corp.
+ * Copyright 2007-2015 UnboundID Corp.
  * All Rights Reserved.
  */
 /*
- * Copyright (C) 2008-2014 UnboundID Corp.
+ * Copyright (C) 2008-2015 UnboundID Corp.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License (GPLv2 only)
