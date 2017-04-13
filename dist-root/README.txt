c6ee823ead17a3fb2fd8862469ff1b1e675e0828
Neil A. Wilson <neilwilson@pingidentity.com>
2017-04-13 12:34:22 -0500

    Consolidated the LDAP SDK to a single edition
    Updated the LDAP SDK so that we no longer build three separate editions
    (Standard Edition, Commercial Edition, and Minimal Edition).  The reasons for
    having three separate editions no longer exist, and it's easier and less
    confusing to just have one edition with everything.

---
+++ dist-root/README.txt
@@ -0,0 +1,16 @@
+UnboundID LDAP SDK for Java
+Copyright 2008-2017 UnboundID Corp.  All Rights Reserved.
+
+This package contains Commercial Edition of the UnboundID LDAP SDK for Java.
+It contains the following elements:
+
+- unboundid-ldapsdk.jar -- The UnboundID LDAP SDK for Java library.
+
+- docs/index.html -- A list of available documentation.
+
+- examples -- A directory containing source code for example programs that
+  illustrate the use of the LDAP SDK.
+
+- LICENSE.txt -- Information about the licenses under which the LDAP SDK may be
+  used.
+
---
+++ dist-root/tools/identify-unique-attribute-conflicts.bat
@@ -0,0 +1,30 @@
+@echo off
+
+rem Copyright 2013-2017 UnboundID Corp.
+rem All Rights Reserved.
+rem
+rem -----
+rem
+rem Copyright (C) 2013-2017 UnboundID Corp.
+rem This program is free software; you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License (GPLv2 only)
+rem or the terms of the GNU Lesser General Public License (LGPLv2.1 only)
+rem as published by the Free Software Foundation.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem
+rem You should have received a copy of the GNU General Public License
+
+
+rem Get the directory containing this batch file.
+set BATDIR=%~dp0
+
+rem Invoke a number of common script utility functions.
+call "%BATDIR%\.script-util.bat"
+
+rem Invoke the tool with the provided command-line arguments.
+"%JAVA_CMD%" %JAVA_ARGS% -cp "%BATDIR%\..\unboundid-ldapsdk.jar;%CLASSPATH%" com.unboundid.ldap.sdk.examples.IdentifyUniqueAttributeConflicts %*
+
