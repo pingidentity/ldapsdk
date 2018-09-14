f4a6462fb8dea16b04928b177f8d8a626287421d
Neil A. Wilson <neilwilson@pingidentity.com>
2018-09-13 22:53:15 -0500

    Post-4.0.8 updates
    Updated the LDAP SDK so that subsequent builds will have a version
    number of 4.0.9, and so that unit tests will ensure backward
    compatibility with the public API exposed by the 4.0.8 release.

--- tests/unit/src/com/unboundid/ldap/sdk/APITestCase.java
+++ tests/unit/src/com/unboundid/ldap/sdk/APITestCase.java
@@ -1411,7 +1411,8 @@ public class APITestCase
       new Object[] { new File(resourceDir, "public-api-4.0.4.txt")  },
       new Object[] { new File(resourceDir, "public-api-4.0.5.txt")  },
       new Object[] { new File(resourceDir, "public-api-4.0.6.txt")  },
-      new Object[] { new File(resourceDir, "public-api-4.0.7.txt")  }
+      new Object[] { new File(resourceDir, "public-api-4.0.7.txt")  },
+      new Object[] { new File(resourceDir, "public-api-4.0.8.txt")  }
     };
   }
 
