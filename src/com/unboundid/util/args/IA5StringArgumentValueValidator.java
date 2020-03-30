c1b493ecee684ff5bebc62948cda5216d1d10c3c
Neil A. Wilson <neilwilson@pingidentity.com>
2020-03-30 15:56:38 -0500

    Add an IA5 string argument value validator
    Added an IA5 string argument value validator that can be used to
    ensure that values for associated arguments are only allowed to
    contain ASCII characters (that is, characters from the IA5 character
    set).  It may optionally be configured to accept or reject empty
    strings.

---
+++ src/com/unboundid/util/args/IA5StringArgumentValueValidator.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2020 Ping Identity Corporation
+ * All Rights Reserved.
+ */
+/*
+ * Copyright 2020 Ping Identity Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Copyright (C) 2020 Ping Identity Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPLv2 only)
+ * or the terms of the GNU Lesser General Public License (LGPLv2.1 only)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ */
+package com.unboundid.util.args;
+
+
+
+import java.io.Serializable;
+
+import com.unboundid.util.NotMutable;
+import com.unboundid.util.ThreadSafety;
+import com.unboundid.util.ThreadSafetyLevel;
+
+import static com.unboundid.util.args.ArgsMessages.*;
+
+
+
+/**
+ * This class provides an implementation of an argument value validator that
+ * ensures that values can be parsed as valid IA5 strings (that is, strings
+ * containing only ASCII characters).
+ */
+@NotMutable()
+@ThreadSafety(level=ThreadSafetyLevel.COMPLETELY_THREADSAFE)
+public final class IA5StringArgumentValueValidator
+       extends ArgumentValueValidator
+       implements Serializable
+{
+  /**
+   * The serial version UID for this serializable class.
+   */
+  private static final long serialVersionUID = 7395996449791650693L;
+
+
+
+  // Indicates whether values are allowed to be empty strings.
+  private final boolean allowEmptyStrings;
+
+
+
+  /**
+   * Creates a new IA5 string argument value validator.  Empty strings will not
+   * be considered valid.
+   */
+  public IA5StringArgumentValueValidator()
+  {
+    this(false);
+  }
+
+
+
+  /**
+   * Creates a new IA5 string argument value validator.
+   *
+   * @param  allowEmptyStrings  Indicates whether empty strings will be
+   *                            considered valid.
+   */
+  public IA5StringArgumentValueValidator(final boolean allowEmptyStrings)
+  {
+    this.allowEmptyStrings = allowEmptyStrings;
+  }
+
+
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override()
+  public void validateArgumentValue(final Argument argument,
+                                    final String valueString)
+         throws ArgumentException
+  {
+    final int length = valueString.length();
+    if ((length == 0) && (! allowEmptyStrings))
+    {
+      throw new ArgumentException(ERR_IA5_STRING_VALIDATOR_EMPTY_STRING.get(
+           argument.getIdentifierString()));
+    }
+
+    for (int i=0; i < length; i++)
+    {
+      final int c = valueString.charAt(i);
+      final int fullByte = (c & 0xFF);
+      final int asciiByte = (c & 0x7F);
+      if (fullByte != asciiByte)
+      {
+        throw new ArgumentException(ERR_IA5_STRING_VALIDATOR_ILLEGAL_CHAR.get(
+             valueString, argument.getIdentifierString(), String.valueOf(c),
+             i));
+      }
+    }
+  }
+
+
+
+  /**
+   * Indicates whether empty strings should be considered valid.
+   *
+   * @return  {@code true} if empty strings should be considered valid, or
+   *          {@code false} if not.
+   */
+  public boolean allowEmptyStrings()
+  {
+    return allowEmptyStrings;
+  }
+
+
+
+  /**
+   * Retrieves a string representation of this argument value validator.
+   *
+   * @return  A string representation of this argument value validator.
+   */
+  @Override()
+  public String toString()
+  {
+    final StringBuilder buffer = new StringBuilder();
+    toString(buffer);
+    return buffer.toString();
+  }
+
+
+
+  /**
+   * Appends a string representation of this argument value validator to the
+   * provided buffer.
+   *
+   * @param  buffer  The buffer to which the string representation should be
+   *                 appended.
+   */
+  public void toString(final StringBuilder buffer)
+  {
+    buffer.append("IA5StringArgumentValueValidator(allowEmptyStrings=");
+    buffer.append(allowEmptyStrings);
+    buffer.append(')');
+  }
+}
---
+++ tests/unit/src/com/unboundid/util/args/IA5StringArgumentValueValidatorTestCase.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2017-2020 Ping Identity Corporation
+ * All Rights Reserved.
+ */
+/*
+ * Copyright 2017-2020 Ping Identity Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Copyright (C) 2017-2020 Ping Identity Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPLv2 only)
+ * or the terms of the GNU Lesser General Public License (LGPLv2.1 only)
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ */
+package com.unboundid.util.args;
+
+
+
+import org.testng.annotations.Test;
+
+import com.unboundid.ldap.sdk.LDAPSDKTestCase;
+
+
+
+/**
+ * This class provides a set of test cases for the IA5 string argument value
+ * validator.
+ */
+public final class IA5StringArgumentValueValidatorTestCase
+       extends LDAPSDKTestCase
+{
+  /**
+   * Tests the behavior of the default constructor.
+   *
+   * @throws Exception If an unexpected problem occurs.
+   */
+  @Test()
+  public void testDefaultConstructor()
+       throws Exception
+  {
+    final IA5StringArgumentValueValidator v =
+         new IA5StringArgumentValueValidator();
+
+    assertFalse(v.allowEmptyStrings());
+
+    assertNotNull(v.toString());
+    assertFalse(v.toString().isEmpty());
+
+
+    final StringArgument arg =
+         new StringArgument(null, "ia5String", true, 1, "{value}",
+              "A test IA5 String.");
+
+    try
+    {
+      v.validateArgumentValue(arg, "");
+      fail("Expected an exception when validating an empty value.");
+    }
+    catch (final ArgumentException e)
+    {
+      // This is expected.
+    }
+
+    v.validateArgumentValue(arg, "this is a valid IA5 string");
+
+    try
+    {
+      v.validateArgumentValue(arg, "jalape\u00f1o");
+      fail("Expected an exception when validating a value with non-ASCII " +
+           "characters.");
+    }
+    catch (final ArgumentException e)
+    {
+      // This is expected.
+    }
+
+    assertNotNull(v.toString());
+  }
+
+
+
+  /**
+   * Tests the behavior of the non-default constructor that is configured to
+   * allow empty strings.
+   *
+   * @throws Exception If an unexpected problem occurs.
+   */
+  @Test()
+  public void testAllowEmptyStrings()
+       throws Exception
+  {
+    final IA5StringArgumentValueValidator v =
+         new IA5StringArgumentValueValidator(true);
+
+    assertTrue(v.allowEmptyStrings());
+
+    assertNotNull(v.toString());
+    assertFalse(v.toString().isEmpty());
+
+
+    final StringArgument arg =
+         new StringArgument(null, "ia5String", true, 1, "{value}",
+              "A test IA5 String.");
+
+    v.validateArgumentValue(arg, "");
+
+    v.validateArgumentValue(arg, "this is a valid IA5 string");
+
+    try
+    {
+      v.validateArgumentValue(arg, "jalape\u00f1o");
+      fail("Expected an exception when validating a value with non-ASCII " +
+           "characters.");
+    }
+    catch (final ArgumentException e)
+    {
+      // This is expected.
+    }
+
+    assertNotNull(v.toString());
+  }
+}
