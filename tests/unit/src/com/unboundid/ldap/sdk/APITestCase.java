14cdbf7b598ae973387da6bb431a31506f1453e4
Neil A. Wilson <neilwilson@pingidentity.com>
2021-03-23 14:20:10 -0500

    Post-5.1.4 release updates
    Updated the LDAP SDK so that new builds will have a version number
    of 5.1.5.  Also, captured the public API exposed by the 5.1.4
    release to ensure that we preserve backwards compatibility with it.

--- tests/unit/src/com/unboundid/ldap/sdk/APITestCase.java
+++ tests/unit/src/com/unboundid/ldap/sdk/APITestCase.java
@@ -1794,7 +1794,8 @@ public class APITestCase
       new Object[] { new File(resourceDir, "public-api-5.1.0.txt")  },
       new Object[] { new File(resourceDir, "public-api-5.1.1.txt")  },
       new Object[] { new File(resourceDir, "public-api-5.1.2.txt")  },
-      new Object[] { new File(resourceDir, "public-api-5.1.3.txt")  }
+      new Object[] { new File(resourceDir, "public-api-5.1.3.txt")  },
+      new Object[] { new File(resourceDir, "public-api-5.1.4.txt")  }
     };
   }
 
