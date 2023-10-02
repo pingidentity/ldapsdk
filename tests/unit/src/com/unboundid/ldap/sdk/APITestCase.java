3dd7837c631e79b08bd2bfd212c123342a5907ac
Neil Wilson <neilwilson@pingidentity.com>
2023-10-02 11:30:01 -0500

    Post-6.0.10 updates
    Updated the LDAP SDK so that subsequent builds will have a version
    number of 6.0.11.  Also, added tests to ensure that we maintain
    backward compatibility with the 6.0.10 release.

--- tests/unit/src/com/unboundid/ldap/sdk/APITestCase.java
+++ tests/unit/src/com/unboundid/ldap/sdk/APITestCase.java
@@ -1805,7 +1805,8 @@ public class APITestCase
       new Object[] { new File(resourceDir, "public-api-6.0.6.txt")  },
       new Object[] { new File(resourceDir, "public-api-6.0.7.txt")  },
       new Object[] { new File(resourceDir, "public-api-6.0.8.txt")  },
-      new Object[] { new File(resourceDir, "public-api-6.0.9.txt")  }
+      new Object[] { new File(resourceDir, "public-api-6.0.9.txt")  },
+      new Object[] { new File(resourceDir, "public-api-6.0.10.txt")  }
     };
   }
 
